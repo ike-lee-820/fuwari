2e3557da83b3ace1aff3231826403ae276bc0ffb
ike-lee-820 <ike-lee@hotmail.com>
2026-05-03 17:37:40 +0800

    Font Changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://ocd.ccwu.cc/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
