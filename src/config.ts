7e985282d3debfcfb6f29601c3b3267ee593985d
ike-lee-820 <ike-lee@hotmail.com>
2026-05-03 17:19:43 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -75,3 +75,4 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// Please select a dark theme, as this blog theme currently only supports dark background color
 	theme: "github-dark",
 };
+
