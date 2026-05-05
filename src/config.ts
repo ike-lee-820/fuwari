ea5113f7d4ac0344c8d7abe840df970eb78fd002
ike-lee-820 <ike-lee@hotmail.com>
2026-05-05 15:25:35 +0800

    更新评论区

--- src/config.ts
+++ src/config.ts
@@ -20,9 +20,9 @@ export const siteConfig: SiteConfig = {
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "哲风壁纸", // Credit text to be displayed
+			url: "https://haowallpaper.com/homeViewLook/15680536157524288", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
